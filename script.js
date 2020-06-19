5e5fa5b6e2adb7b81dc2703f7fcc949e8da8a184
smitp0 <smit.patel.321@gmail.com>
2020-06-19 13:10:07 -0400

    scroll and redirects added

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ const navbar = document.querySelector(".navbar");
 menuIcon.addEventListener("click", () => {
   navbar.classList.toggle("change");
 })
+var scroll = new SmoothScroll('a[href*="#"]', {
+	speed: 700,
+	speedAsDuration: true
+});
